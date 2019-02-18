fbd333e409b473e22823005da3ac74eb93b4c3e3
Nicholas Keuning <nkeuning@umich.edu>
2019-02-18 15:24:26 -0500

    Initial commit.

---
+++ GetIP/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Networking.Connectivity;
+using Windows.Networking;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace GetIP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.AddressDisplay.Text = this.IPAddress;
+        }
+
+        public string IPAddress
+        {
+            get
+            {
+                var res = NetworkInformation.GetHostNames()
+                    .Where(hn => hn.IPInformation != null)
+                    .Where(hn => hn.Type == HostNameType.Ipv4)
+                    .FirstOrDefault();
+
+                return res?.ToString();
+            }
+        }
+    }
+}
